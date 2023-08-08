e99336403ed329b189037ddcb815202affc75e44
Jewel Dacion <dacionjewelshayne01@gmail.com>
2023-08-08 07:44:55 -0400

    refractor

--- cypress/fixtures/pom/page.js
+++ cypress/fixtures/pom/page.js
@@ -1,17 +1,7 @@
 export default class {
-  get buttonSignIn() {
-    return cy.contains('Sign In');
-  }
-  get inputEmail() {
-    return cy.get('[name="email"]');
-  }
-  get inputPassword() {
-    return cy.get('[name="password"]');
-  }
-  get buttonSubmit() {
-    return cy.get('[type="submit"]');
-  }
-  get buttonLogout() {
-    return cy.get('div span').contains('Log out');
-  }
+  get buttonSignIn() {return cy.contains('Sign In')}
+  get inputEmail() {return cy.get('[name="email"]')}
+  get inputPassword() {return cy.get('[name="password"]')}
+  get buttonSubmit() {return cy.get('[type="submit"]')}
+  get buttonLogout() {return cy.get('div span').contains('Log out')}
 }
