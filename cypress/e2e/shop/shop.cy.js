572a137c20e072f5ea3f8cb5dc895a52eb12cc9a
Jewel Dacion <dacionjewelshayne01@gmail.com>
2023-10-10 09:41:54 -0400

    refractor

--- cypress/e2e/shop/shop.cy.js
+++ cypress/e2e/shop/shop.cy.js
@@ -5,12 +5,19 @@ describe('Shop', () => {
     cy.visit('/');
   });
 
-  it('Shop', () => {
-    cy.contains('shop').click();
-  //   cy.get('[class="Flex-sc-1vyhscq-0 cTWGkP"]').eq(0).click();
-  //   cy.get('.StyleContainer-sc-1dgtedb-0').eq(0).click();
-  //   cy.contains('sleep').click();
-  //   cy.get('[name="collection"]').contains('Classic').click();
+  it('Should be able to add item in the cart without signing in', () => {
+    cy.get('nav span').contains('shop').trigger('mouseover', {force: true})
+    cy.contains('Hiphugger').click({force: true})
+    cy.get('.DropletLink__StyledDroplets-sc-18jej1t-0').eq(0).click()
+    cy.get('[data-testid="color-swatch-Black"]').click()
+    cy.get('div label').contains('XS').click()
+    cy.get('#productQuantity').should('have.value', '1')
+    cy.get('[type="button"]').contains('style + fit').click()
+    cy.contains('add to bag').click()
+    cy.get('#cart-preview-trigger').trigger('mouseover')
+    cy.contains('view bag').click({force: true})
+    cy.get('[aria-label="product in cart"]').contains('Super Hiphugger').should('be.visible')
+    cy.get('[aria-label="product in cart"]').contains('XS / Black').should('be.visible')
  
   });
 });
---
+++ cypress/fixtures/pom/shop/shop.page.js
@@ -0,0 +1,7 @@
+import page from "../page"
+
+class shopPage extends page{
+   
+}
+
+export default new shopPage()
